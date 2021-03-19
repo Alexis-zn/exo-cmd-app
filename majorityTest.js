34b6416214cb416d95cf41d35884bdd89aa62a92
Alexis-zn <alexis.zentici@gmail.com>
2021-03-19 12:08:53 +0100

    update

--- majorityTest.js
+++ majorityTest.js
@@ -1,31 +1,35 @@
-// Ne marche pas à 100% pour le moment !
-
 const readlineSync = require('readline-sync')
 
-let name = readlineSync.question('Your Name: ')
-let surname = readlineSync.question('Your Surname: ')
-let age = readlineSync.question('Your Age: ')
+// Check command line
+if (process.argv.length !== 3) {
+  console.log('usage: node majorityTest.js age')
+  process.exit(1)
+}
 
-age = Number(age)
+if (isNaN(process.argv[2])) {
+  console.log(`Error: ${process.argv[2]} is not a number`)
+  process.exit(1)
+}
 
-// Stop the program if name length's is too short or if it's a number.
-if(name.length < 2) {
-  console.log('Error: incorrect format. ')
-  process.exit(1)
+const majority = Number(process.argv[2])
+
+while (true) {
+  const name = readlineSync.question('nom: ')
+  const firstName = readlineSync.question('firstName: ')
+  const ageStr = readlineSync.question('age: ')
+
+  if (isNaN(ageStr)) {
+    console.log(`Sorry, ${ageStr} is not a number`)
+    process.exit(1)
+  }
+
+  const age = Number(ageStr)
+
+  if (age >= majority) {
+    console.log(`${firstName} ${name} vous êtes majeur, vous pouvez voter.`)
+  } else {
+    console.log(
+      `Désolé, ${firstName} ${name}, vous êtes mineur, vous ne pouvez pas voter.`
+    )
+  }
 }
-// Stop the program if surname length's is too short or if it's a number.
-if(surname.length < 2) {
-  console.log('Error: incorrect format. ') 
-  process.exit(1)
-}
-// Stop the program if age is not a number.
-if(isNaN(age)) {
-  console.log('Error: incorrect format.') 
-  process.exit(1)
-}
-// Check if age is > or < 18.
-/* if(age > 18) {
-  console.log(`${name} ${surname}, vous êtes majeur, vous pouvez voter :) !`)
-} else {
-  console.log(`${name} ${surname}, vous êtes mineur, vous ne pouvez pas voter :( ! `)
-} */
